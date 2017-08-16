9ecc9f61852df8ebfad3688aca243c215b48a2d3
marcuscf <marcuscf@home>
2017-08-16 00:31:03 -0300

    Basic test
    It runs, but doesn't check the result (it would fail anyway)

---
+++ tests/01_IN_indent.lua
@@ -0,0 +1,44 @@
+-- Well structured code
+tostring(self.x)
+for i in x do
+if x then
+while 1 do
+local a = function()
+if 1 then
+if 1 then
+call()
+end
+elseif 1 then
+call()
+if 1 then
+call()
+else
+call_2()
+end
+else
+call()
+end
+while 3 do
+call()
+end
+repeat
+x()
+until y
+end
+local tbl = {
+a = function()
+do call() end
+if 1 TODO then
+call()
+elseif TODO then
+call_2()
+else
+TODO
+call()
+call()
+end
+end
+}
+end
+end
+end
--- tests/run-tests.lua
+++ tests/run-tests.lua
@@ -1,20 +1,42 @@
-local input_file_name = arg[1]
-local output_file_name = arg[2]
+#!/usr/bin/lua
 
-local shiftwidth = 4
+function run_vim(input_file_name, output_file_name)
+    sanitize_file_name(input_file_name)
+    sanitize_file_name(output_file_name)
 
-local vim = 'vim -u NONE -i NONE'
+    local shiftwidth = 4
 
-local vim_init = string.format([[
--c set nocompatible lazyredraw
--c edit %s
--c source ../indent/lua.vim
--c set shiftwidth=%d expandtab]], input_file_name, shiftwidth)
+    local vim = 'vim -u NONE -i NONE'
 
-local vim_indent = '-c normal ggVG='
+    local vim_init = string.format([[
+        -c 'set nocompatible lazyredraw'
+        -c 'edit %s'
+        -c 'source ../indent/lua.vim'
+        -c 'set shiftwidth=%d expandtab']], input_file_name, shiftwidth)
 
-local vim_save = string.format('-c write! %s -c qa!', output_file_name)
+    vim_init = string.gsub(vim_init, '%s+', ' ')
 
-local vim_full_command = string.format('%s %s %s %s', vim, vim_init, vim_indent, vim_save)
+    local vim_indent = [[-c 'normal ggVG=']]
 
-local success, return_type, return_number = os.execute(vim_full_command)
+    local vim_save = string.format([[-c 'write! %s' -c 'qa!']], output_file_name)
+
+    local vim_full_command = string.format('%s %s %s %s', vim, vim_init, vim_indent, vim_save)
+
+    print('Running: ' .. vim_full_command)
+    local success, return_type, return_number = os.execute(vim_full_command)
+
+    print(string.format('Success: %s, return type: %s, return code: %d', success, return_type, return_number))
+end
+
+function sanitize_file_name(file_name)
+    -- This is mostly to avoid quoting problems, but being a little bit
+    -- stricter than that may be safer and avoid nasty surprises in the future.
+    if not string.match(file_name, '^[%w._-]+$') then
+        error('Invalid file name: ' .. file_name)
+    end
+end
+
+local input_file_name = '01_IN_indent.lua' -- arg[1]
+local output_file_name = '01_OUT_indent.lua' -- arg[2]
+
+run_vim(input_file_name, output_file_name)
